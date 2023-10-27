9f93b95a370b91238dba9c929924078d957c33e2
Mabel Pertot <mabelgem77@gmail.com>
2023-10-27 03:04:13 -0300

    Actualizaciones

--- js/script.js
+++ js/script.js
@@ -1,74 +1,82 @@
-function toggleInfo(id) {
-   info = document.getElementById(id);
-  if (info.style.display === "none" || info.style.display === "") {
-    info.style.display = "block"; // Mostrar la información adicional
-  } else {
-    info.style.display = "none"; // Ocultar la información adicional
-  }
-}
 
-    // Mostrar u ocultar la información adicional al hacer clic en el botón.
-    function toggleInfo(infoId) {
-       info = document.getElementById(infoId);
-      if (info.style.display === "none") {
-        info.style.display = "block";
+  document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("registration-form").addEventListener("submit", function (event) {
+      event.preventDefault();
+      enviar();
+    });
+
+    function enviar() {
+      const nombre = document.getElementById("nombre").value;
+      const apellido = document.getElementById("apellido").value;
+      const direccion = document.getElementById("direccion").value;
+      const numero = document.getElementById("numero").value;
+      const codigoPostal = document.getElementById("codigo-postal").value;
+      const telefono = document.getElementById("telefono").value;
+      const email = document.getElementById("email").value;
+      const confirmarEmail = document.getElementById("confirmar-email").value;
+      const contrasena = document.getElementById("contrasena").value;
+      const confirmarContrasena = document.getElementById("confirmar-contrasena").value;
+      textarea = document.getElementById("textarea").value;
+
+      if (nombre && apellido && direccion && numero && codigoPostal && telefono && email && confirmarEmail && contrasena && confirmarContrasena) {
+        if (contrasena === confirmarContrasena && email === confirmarEmail) {
+          alert("¡El registro fue exitoso! A la brevedad nos pondremos en contacto.");
+          // limpia elformulario
+          document.getElementById("nombre").value = "";
+          document.getElementById("apellido").value = "";
+          document.getElementById("direccion").value = "";
+          document.getElementById("numero").value = "";
+          document.getElementById("codigo-postal").value = "";
+          document.getElementById("telefono").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("confirmar-email").value = "";
+          document.getElementById("contrasena").value = "";
+          document.getElementById("confirmar-contrasena").value = "";
+          document.getElementById("textarea").value = "";
+        } else {
+          alert("Las contraseñas o los emails no coinciden.");
+        }
       } else {
-        info.style.display = "none";
+        alert("Por favor, complete todos los campos obligatorios.");
       }
     }
-
-// Función para limpiar el formulario y ocultar el mensaje
-  document.addEventListener("DOMContentLoaded", function () {
-    // Referencias a los elementos del botón y la caja
-     moreInfoButton = document.getElementById("more-info-button");
-     moreInfoBox = document.getElementById("more-info-box");
-
-    // Evento de clic al botón para alternar la visibilidad de la caja
-    moreInfoButton.addEventListener("click", function () {
-      if (moreInfoBox.style.display === "none" || moreInfoBox.style.display === "") {
-        moreInfoBox.style.display = "block";
-      } else {
-        moreInfoBox.style.display = "none";
-      }
-    });
   });
 
-  function enviar() {
-     nombre = document.getElementById("name").value;
-     fecha_nacimiento = document.getElementById("fecha_nacimiento").value;
-     dni = document.getElementById("dni").value;
-     phone = document.getElementById("phone").value;
-     email = document.getElementById("email").value;
-     message = document.getElementById("message").value;
-     contrasena = document.getElementById("contrasena").value;
-     confirmarContrasena = document.getElementById("confirmar-contrasena").value;
-     
-    
-    if (nombre && fecha_nacimiento && dni && phone && email && message && contrasena && confirmarContrasena) {
-      if (contrasena === confirmarContrasena) {
+  function toggleInfo(id) {
+    info = document.getElementById(id);
+   if (info.style.display === "none" || info.style.display === "") {
+     info.style.display = "block"; // Mostrar la información adicional
+   } else {
+     info.style.display = "none"; // Ocultar la información adicional
+   }
+ }
+ 
+     // Mostrar u ocultar la información adicional al hacer clic en el botón.
+     function toggleInfo(infoId) {
+        info = document.getElementById(infoId);
+       if (info.style.display === "none") {
+         info.style.display = "block";
+       } else {
+         info.style.display = "none";
+       }
+     }
+ 
+  
 
-        
 
 
 
-        alert("¡Su mensaje fue recibido correctamente! A la brevedad nos pondremos en contacto.");
-      
-      // Limpiar el formulario
-      document.getElementById("name").value = "";
-      document.getElementById("fecha_nacimiento").value = "";
-      document.getElementById("dni").value = "";
-      document.getElementById("phone").value = "";
-      document.getElementById("email").value = "";
-      document.getElementById("contrasena").value = "";
-      document.getElementById("confirmar-contrasena").value = "";
-      document.getElementById("message").value = "";
-    } else {
-      alert("Las contraseñas no coinciden. Por favor, inténtelo de nuevo.");
-    }
-  }else {
-    alert("Por favor, complete todos los campos obligatorios.");
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
 
 
  
